d9434d0a489d613eca1e2c897099f081a124a681
Jacob D'Agostino <jacob.dagostino@proton.me>
2025-07-15 21:00:03 -0700

    refactored coin toss

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 use rand::Rng;
+use std::fmt;
+
+struct Divination {
+    cointoss: bool 
+}
+
+impl Divination {
+    // the result is locked in on creation, much like your own fate in real life
+    fn new() -> Self {
+        let mut rng = rand::rng();
+
+        let cointoss = rng.random_bool(0.5);
+        
+        Divination {cointoss} 
+    }
+}
+
+impl fmt::Display for Divination {
+    // emit text
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.cointoss == true {
+            write!(f, "Heads! Go for it")?;
+        } else {
+            write!(f, "Tails! Probably means 'no'.")?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() {
-
-    let mut rng = rand::rng();
-
     let mut input = String::new();
     
     println!("Think deeply on your question and press Enter when ready...");
     
-    std::io::stdin().read_line(&mut input).expect("whoops");
+    std::io::stdin().read_line( &mut input).expect("whoops");
 
-    let coin = rng.random_bool(0.5);
-    if coin == true {
-        println!("Heads! Go for it");
-    } else {
-        println!("Tails! Probably means 'no'.")
-    }
+    let div = Divination::new();
+    println!("{}",div);
+
 }
